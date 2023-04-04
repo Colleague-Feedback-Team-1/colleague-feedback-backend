af155ee700fffb87a8726716ba2a088b9e147b83
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-04 13:09:34 +0300

    Added user input sanitization and duplication checks

--- src/controllers/requests.ts
+++ src/controllers/requests.ts
@@ -3,10 +3,9 @@ import createHttpError from 'http-errors'
 import RequestsModel from '../data_models/request'
 import { RequestsI, ReviewerI } from '../data_models/request'
 import validator from 'validator'
-import { isValidEmail, isValidUsername } from '../utils/validators'
 import mongoose from 'mongoose'
 
-//needs validation and sanitazation
+//Get all
 export const getAll: RequestHandler = async (req, res, next) => {
   try {
     const toBeReviewedAll = await RequestsModel.find().exec()
@@ -19,7 +18,7 @@ export const getAll: RequestHandler = async (req, res, next) => {
 
 //Get one employee by an id
 export const getRequestById: RequestHandler = async (req, res, next) => {
-  const requestid = req.params.requestid
+  const requestid = validator.escape(req.params.requestid)
   try {
     if (!mongoose.isValidObjectId(requestid)) {
       throw createHttpError(400, `Request id: ${requestid} is invalid `)
@@ -34,8 +33,7 @@ export const getRequestById: RequestHandler = async (req, res, next) => {
     next(error)
   }
 }
-
-//Insert Request
+//Insert a new request
 export const insertRequest: RequestHandler<unknown, unknown, RequestsI, unknown> = async (
   req,
   res,
@@ -54,21 +52,34 @@ export const insertRequest: RequestHandler<unknown, unknown, RequestsI, unknown>
       reviewers,
     } = req.body
 
-    // Create a new instance of the RequestModel with the extracted data
+    //Multiple requests?
+    const existingRequest = await RequestsModel.findOne({ employeeid }).exec()
+    if (existingRequest) {
+      throw createHttpError(409, `A request with employee id: ${employeeid} already exists`)
+    }
+    // Validate input data
+    if (!validator.isEmail(employeeEmail)) {
+      throw createHttpError(400, 'Employee email is not valid')
+    }
+
+    // Sanitize input data
+    const sanitizedEmployeeName = validator.trim(employeeName)
+    const sanitizedEmployeeEmail = validator.normalizeEmail(employeeEmail)
+    const sanitizedAssignedManagerName = validator.trim(assignedManagerName)
+
     const employeeToBeReviewed = RequestsModel.create({
       employeeid: employeeid,
-      employeeName: employeeName,
-      employeeEmail: employeeEmail,
+      employeeName: sanitizedEmployeeName,
+      employeeEmail: sanitizedEmployeeEmail,
       assignedManagerid: assignedManagerid,
-      assignedManagerName: assignedManagerName,
+      assignedManagerName: sanitizedAssignedManagerName,
       confirmedByHR: confirmedByHR,
       selfReview: selfReview,
       dateRequested: dateRequested,
       reviewers,
     })
-    res.status(201).json(employeeToBeReviewed)
+    res.status(200).json(employeeToBeReviewed)
   } catch (error) {
-    // Pass any errors to the error handling middleware
     next(error)
   }
 }
@@ -91,39 +102,13 @@ export const deleteRequest: RequestHandler = async (req, res, next) => {
   }
 }
 
-//Update self review status
-// export const updateSelfReviewStatus: RequestHandler<
-//   { requestid: string },
-//   unknown,
-//   { selfReview: boolean },
-//   unknown
-// > = async (req, res, next) => {
-//   const requestid = req.params.requestid
-//   const newSelfReview = req.body.selfReview
-
-//   try {
-//     if (!mongoose.isValidObjectId(requestid)) {
-//       throw createHttpError(400, `Request id: ${requestid} is invalid `)
-//     }
-//     const request = await RequestsModel.findById(requestid).exec()
-//     if (!request) {
-//       throw createHttpError(404, `Request with ${requestid} not found`)
-//     }
-//     request.selfReview = newSelfReview
-//     const updatedEmployeeData = await request.save()
-
-//     res.status(200).json(updatedEmployeeData)
-//   } catch (error) {
-//     next(error)
-//   }
-// }
 export const updateSelfReviewStatus: RequestHandler<
   { requestid: string },
   unknown,
   { selfReview?: boolean; confirmedByHR?: boolean },
   unknown
 > = async (req, res, next) => {
-  const requestid = req.params.requestid
+  const requestid = validator.escape(req.params.requestid)
   const { selfReview, confirmedByHR } = req.body
 
   try {
@@ -157,25 +142,52 @@ export const insertReviewers: RequestHandler<
   ReviewerI[],
   unknown
 > = async (req, res, next) => {
-  const requestid = req.params.requestid
+  const requestid = validator.escape(req.params.requestid)
   const newReviewers = req.body
+
   try {
     if (!mongoose.isValidObjectId(requestid)) {
-      throw createHttpError(400, `Request id: ${requestid} is invalid `)
+      throw createHttpError(400, `Request id: ${requestid} is invalid`)
     }
+
     const request = await RequestsModel.findById(requestid).exec()
     if (!request) {
       throw createHttpError(404, `Request with ${requestid} was not found`)
     }
+
+    // Sanitize newReviewers array using validator
+    const sanitizedReviewers = newReviewers.map((reviewer) => {
+      return {
+        reviewerid: validator.escape(reviewer.reviewerid),
+        reviewerName: validator.escape(reviewer.reviewerName),
+        reviewerEmail: validator.normalizeEmail(reviewer.reviewerEmail),
+        role: validator.escape(reviewer.role),
+        image: validator.escape(reviewer.image),
+        feedbackSubmitted: reviewer.feedbackSubmitted,
+      }
+    })
+    //Duplication check
+    const currentReviewers = request.reviewers.map((reviewer) => reviewer.reviewerid)
+    const duplicateReviewers = sanitizedReviewers.filter((reviewer) =>
+      currentReviewers.includes(reviewer.reviewerid)
+    )
+    if (duplicateReviewers.length > 0) {
+      throw createHttpError(
+        400,
+        `Duplicate reviewers found with ids: ${duplicateReviewers
+          .map((reviewer) => reviewer.reviewerid)
+          .join(', ')}`
+      )
+    }
+
     const updatedRequest: RequestsI | null = await RequestsModel.findByIdAndUpdate(
       requestid,
-      { $push: { reviewers: { $each: newReviewers } } },
+      { $push: { reviewers: { $each: sanitizedReviewers } } },
       { new: true }
     ).exec()
+
     if (updatedRequest) {
       res.status(200).json(updatedRequest)
-    } else {
-      res.status(404).send('Request not found')
     }
   } catch (error) {
     next(error)
@@ -188,30 +200,28 @@ export const updateFeedbackSubmittedStatus: RequestHandler<
   { feedbackSubmitted: boolean },
   unknown
 > = async (req, res, next) => {
-  const { requestid, reviewerid } = req.params;
-  const { feedbackSubmitted } = req.body;
+  const requestid = validator.escape(req.params.requestid)
+  const reviewerid = validator.escape(req.params.requestid)
+
+  const { feedbackSubmitted } = req.body
 
   try {
     if (!mongoose.isValidObjectId(requestid)) {
-      throw createHttpError(400, `Request id: ${requestid} is invalid`);
+      throw createHttpError(400, `Request id: ${requestid} is invalid`)
     }
     if (!mongoose.isValidObjectId(reviewerid)) {
-      throw createHttpError(400, `Reviewer id: ${reviewerid} is invalid`);
+      throw createHttpError(400, `Reviewer id: ${reviewerid} is invalid`)
     }
     const request = await RequestsModel.findOneAndUpdate(
       { _id: requestid, 'reviewers._id': reviewerid },
       { $set: { 'reviewers.$.feedbackSubmitted': feedbackSubmitted } },
       { new: true }
-    ).exec();
+    ).exec()
     if (!request) {
-      throw createHttpError(404, `Request with id ${requestid} was not found`);
+      throw createHttpError(404, `Request with id ${requestid} was not found`)
     }
-    res.status(200).json(request);
+    res.status(200).json(request)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
-
-
-
-
+}
