2af9ec26359c8598677f9a83bdda8985446dedc3
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-10 11:57:57 +0300

    Endpoints for Feedback Data

--- src/routes/feedbackData.ts
+++ src/routes/feedbackData.ts
@@ -3,6 +3,10 @@ import * as feedbackDataController from '../controllers/feedbackData'
 
 const router = express.Router()
 
+//get all feedback data
 router.get('/', feedbackDataController.getAll)
-
+//insert feedback data 
+router.post('/insert-feedback', feedbackDataController.insertFeedbackData)
+//update feedback data 
+router.patch('/insert-feedback', feedbackDataController.insertFeedbackData)
 export default router
