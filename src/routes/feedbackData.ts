93a057300e7b0717decf070509be38253db45398
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-10 16:30:38 +0300

    Separated endopoint to post and patch

--- src/routes/feedbackData.ts
+++ src/routes/feedbackData.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', feedbackDataController.getAll)
 //insert feedback data OR update feedback data
 router.post('/insert-feedback', feedbackDataController.insertFeedbackData)
-router.patch('/insert-feedback', feedbackDataController.insertFeedbackData)
+router.patch('/insert-feedback/:requestid', feedbackDataController.updateFeedbackData)
 
 //find data by request id
 router.get('/:requestid', feedbackDataController.getFeedbackDataByRequestId)
