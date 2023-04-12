e6d24439139da4992b4079bfbe71eb1f5ea003db
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-12 11:41:57 +0300

    Updated employee model

--- src/data_models/employee.ts
+++ src/data_models/employee.ts
@@ -1,19 +1,19 @@
 import { InferSchemaType, Schema, model } from 'mongoose'
 
 export interface SignUpBody {
-  employeeName: string
-  employeeEmail: string
+  employeeName?: string
+  employeeEmail?: string
   password: string
-  companyRole: string
-  profilePicture: string
+  companyRole?: string
+  profilePicture?: string
   privileges: 'Admin' | 'User' | 'Manager'
 }
 
 const employeeSchema = new Schema({
-  employeeName: { type: String, required: true, unique: true },
-  employeeEmail: { type: String, required: true, unique: true },
+  employeeName: { type: String, required: true },
+  employeeEmail: { type: String, required: true, unique:true },
   password: { type: String, required: true, select: false },
-  companyRole: { type: String, required: true, unique: true },
+  companyRole: { type: String, required: true},
   profilePicture: { type: String },
   privileges: {
     type: String,
