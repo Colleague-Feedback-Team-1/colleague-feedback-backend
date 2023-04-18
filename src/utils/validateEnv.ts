3bca2a699ca15efe7be79b6634e334a49ab14309
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-18 12:52:12 +0300

    added ldap

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -5,4 +5,8 @@ export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
   PORT: port(),
   SESSION_SECRET: str(),
+  LDAP_URL: str(),
+  LDAP_ADMIN_DN: str(),
+  LDAP_BASE_DN: str(),
+  LDAP_ADMIN_PASSWORD: str(),
 })
