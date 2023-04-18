7069e4139045bc6ef864c585ddbcbe46c3b44b5e
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-18 19:45:47 +0300

    removed commeted out code

--- src/controllers/employees.ts
+++ src/controllers/employees.ts
@@ -4,6 +4,7 @@ import EmployeeModel from '../data_models/employee'
 import validator from 'validator'
 import mongoose from 'mongoose'
 
+//Needs regex expression to extract uid from dn attribute-value pairs...
 const extractUidFromDn = (dn: string): string | null => {
   const regex = /uid=([^,]+),/;
   const match = regex.exec(dn);
@@ -35,8 +36,6 @@ export const getAuthenticatedEmployee: RequestHandler = async (req, res, next) =
   }
 };
 
-
-
 export const getEmployeeById: RequestHandler<{ id: string }, unknown, unknown, unknown> = async (
   req,
   res,
@@ -67,82 +66,6 @@ export const getAllEmployees: RequestHandler = async (req, res, next) => {
     next(error)
   }
 }
-
-// export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = async (
-//   req,
-//   res,
-//   next
-// ) => {
-//   const { employeeName, employeeEmail, password, companyRole, profilePicture, privileges } =
-//     req.body
-//   //No validation included as the function will be only used in production
-//   try {
-//     // Hash password and create a new user
-//     const passwordHashed = await bcrypt.hash(password, 10)
-//     const newEmployee = await EmployeeModel.create({
-//       employeeName: employeeName,
-//       employeeEmail: employeeEmail,
-//       password: passwordHashed,
-//       companyRole: companyRole,
-//       profilePicture: profilePicture,
-//       privileges: privileges,
-//     })
-
-//     // Store session data
-//     req.session.userId = newEmployee._id
-//     res.status(201).json(newEmployee)
-//   } catch (error) {
-//     next(error)
-//   }
-// }
-
-// interface LoginBody {
-//   employeeEmail: string
-//   password: string
-// }
-// export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (
-//   req,
-//   res,
-//   next
-// ) => {
-//   const { employeeEmail, password } = req.body
-//   const sanitizedEmail = validator.escape(employeeEmail)
-//   const sanitizedPassword = validator.escape(password).trim()
-
-//   try {
-//     if (!sanitizedEmail || !sanitizedPassword) {
-//       throw createHttpError(400, 'Parameters missing')
-//     }
-
-//     if (!isValidEmail(sanitizedEmail)) {
-//       throw createHttpError(400, 'Invalid email format')
-//     }
-
-//     const user = await EmployeeModel.findOne({ employeeEmail: { $eq: sanitizedEmail } })
-//       .select('+password +employeeName')
-//       .exec()
-
-//     if (!user) {
-//       throw createHttpError(401, 'Invalid credentials')
-//     }
-
-//     const passwordMatch = await bcrypt.compare(sanitizedPassword, user.password)
-//     if (!passwordMatch) {
-//       throw createHttpError(401, 'Invalid credentials')
-//     }
-
-//     // Store session data
-//     req.session.userId = user._id
-
-//     //Ignore user password
-//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//     const { password: _, ...userRes } = user.toObject()
-
-//     res.status(200).json(userRes)
-//   } catch (error) {
-//     next(error)
-//   }
-// }
 
 export const logout: RequestHandler = (req, res, next) => {
   req.session.destroy((error) => {
