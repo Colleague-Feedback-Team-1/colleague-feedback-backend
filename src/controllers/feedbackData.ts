4bf1d3787667447539967b7d7fb978edbf580962
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-04-24 14:35:12 +0300

    Refactored Insert and Update functions

--- src/controllers/feedbackData.ts
+++ src/controllers/feedbackData.ts
@@ -22,7 +22,6 @@ export const getFeedbackDataByRequestId: RequestHandler<{ requestid: string }> =
   const requestId = validator.escape(req.params.requestid)
   try {
     const feedbackData = await feedbackDataModel.findOne({ requestId }).exec()
-
     res.status(200).json(feedbackData)
   } catch (error) {
     next(error)
@@ -41,6 +40,63 @@ interface FeedbackRequestI {
   }[]
 }
 
+type ScoreAndOpenFeedback = {
+  scores: number[]
+  openFeedback: string[]
+}
+
+const calculateAverageScore = (scores: number[]): number =>
+  scores.reduce((a, b) => a + b, 0) / scores.length
+
+const extractScoresAndOpenFeedback = (
+  questions: FeedbackRequestI['sections'][0]['questions']
+): ScoreAndOpenFeedback =>
+  questions.reduce<ScoreAndOpenFeedback>(
+    (acc, question) => {
+      if (question.score !== undefined) {
+        acc.scores.push(question.score)
+      }
+      if (question.openFeedback !== undefined) {
+        acc.openFeedback.push(question.openFeedback)
+      }
+      return acc
+    },
+    { scores: [], openFeedback: [] }
+  )
+
+const createAnswerScore = (section: FeedbackRequestI['sections'][0]): AnswerScoreI => {
+  const { scores, openFeedback } = extractScoresAndOpenFeedback(section.questions)
+  const averageScore = calculateAverageScore(scores)
+  return { average: averageScore, openFeedback }
+}
+
+const createOrUpdateAnswerBySection = (
+  answersBySection: AnswerBySectionI[],
+  section: FeedbackRequestI['sections'][0],
+  answerScore: AnswerScoreI
+): AnswerBySectionI[] => {
+  const existingSectionIndex = answersBySection.findIndex(
+    (answerBySection) => answerBySection.sectionName === section.sectionName
+  )
+
+  if (existingSectionIndex !== -1) {
+    answersBySection[existingSectionIndex].score.push(answerScore)
+  } else {
+    answersBySection.push({
+      sectionName: section.sectionName,
+      score: [answerScore],
+    })
+  }
+
+  return answersBySection
+}
+
+const createAnswersBySection = (sections: FeedbackRequestI['sections']): AnswerBySectionI[] =>
+  sections.reduce<AnswerBySectionI[]>((acc, section) => {
+    const answerScore = createAnswerScore(section)
+    return createOrUpdateAnswerBySection(acc, section, answerScore)
+  }, [])
+
 export const insertFeedbackData: RequestHandler<
   unknown,
   unknown,
@@ -50,65 +106,27 @@ export const insertFeedbackData: RequestHandler<
   const { requestid, employeeid, sections } = req.body
   const sanitizedRequestId = validator.escape(requestid)
   const sanitizedEmployeeId = validator.escape(employeeid)
+
   if (!validateFeedbackData(requestid, employeeid, sections)) {
     return res.status(400).send('Invalid input data')
   }
 
-  const answersBySection: AnswerBySectionI[] = []
-
-  // Calculate average scores for each section
-  for (const section of sections) {
-    const scores: number[] = []
-    const openFeedback: string[] = []
-
-    for (const question of section.questions) {
-      if (question.score !== undefined) {
-        scores.push(question.score)
-      }
-      if (question.openFeedback !== undefined) {
-        openFeedback.push(question.openFeedback)
-      }
-    }
-
-    const averageScore: number = scores.reduce((a, b) => a + b, 0) / scores.length
-
-    const answerScore: AnswerScoreI = {
-      average: averageScore,
-      openFeedback: openFeedback,
-    }
-
-    // Store scores by section name
-    const existingSectionIndex = answersBySection.findIndex(
-      (answerBySection) => answerBySection.sectionName === section.sectionName
-    )
-
-    if (existingSectionIndex !== -1) {
-      answersBySection[existingSectionIndex].score.push(answerScore)
-    } else {
-      const answerBySection: AnswerBySectionI = {
-        sectionName: section.sectionName,
-        score: [answerScore],
-      }
-      answersBySection.push(answerBySection)
-    }
-  }
-
-  // Check if feedback data with the given requestId already exists
-  const existingFeedbackData = await feedbackDataModel.findOne({ requestId: sanitizedRequestId })
-
-  if (existingFeedbackData) {
-    res.status(409).send('Feedback data with this requestid already exists')
-    return
-  }
-
-  // Create new feedback data
-  const newFeedbackData: FeedbackDataI = {
-    requestId: sanitizedRequestId,
-    employeeId: sanitizedEmployeeId,
-    answersBySection: answersBySection,
-  }
+  const answersBySection = createAnswersBySection(sections)
 
   try {
+    const existingFeedbackData = await feedbackDataModel.findOne({ requestId: sanitizedRequestId })
+
+    if (existingFeedbackData) {
+      res.status(409).send('Feedback data with this requestid already exists')
+      return
+    }
+
+    const newFeedbackData: FeedbackDataI = {
+      requestId: sanitizedRequestId,
+      employeeId: sanitizedEmployeeId,
+      answersBySection,
+    }
+
     await feedbackDataModel.create(newFeedbackData)
     res.sendStatus(201)
   } catch (error) {
@@ -126,75 +144,30 @@ export const updateFeedbackData: RequestHandler<
   const { requestid, employeeid, sections } = req.body
   const sanitizedRequestId = validator.escape(requestid)
   const sanitizedEmployeeId = validator.escape(employeeid)
+
   if (!validateFeedbackData(requestid, employeeid, sections)) {
     return res.status(400).send('Invalid input data')
   }
-  const answersBySection: AnswerBySectionI[] = []
 
-  // Check if the request contains data
   if (!sections || sections.length === 0) {
     return res.sendStatus(400)
   }
 
-  // Loop through each section in the request
-  sections.forEach((section) => {
-    const { questions } = section
-    const scores: number[] = []
-    const openFeedback: string[] = []
+  const answersBySection = createAnswersBySection(sections)
 
-    // Loop through each question in the section
-    questions.forEach((question) => {
-      if (question.score !== undefined) {
-        scores.push(question.score)
-      }
-      if (question.openFeedback !== undefined) {
-        openFeedback.push(question.openFeedback)
-      }
-    })
-
-    // Calculate the average score for the section
-    const averageScore = scores.reduce((a, b) => a + b, 0) / scores.length
-
-    // Create an AnswerScoreI object for the section
-    const answerScore: AnswerScoreI = {
-      average: averageScore,
-      openFeedback: openFeedback,
-    }
-
-    // Check if the section already exists in answersBySection array
-    const existingSectionIndex = answersBySection.findIndex(
-      (answerBySection) => answerBySection.sectionName === section.sectionName
-    )
-
-    if (existingSectionIndex !== -1) {
-      // Add the AnswerScore object to the existing section
-      answersBySection[existingSectionIndex].score.push(answerScore)
-    } else {
-      // Create a new AnswerBySection object for the section
-      const answerBySection: AnswerBySectionI = {
-        sectionName: section.sectionName,
-        score: [answerScore],
-      }
-      answersBySection.push(answerBySection)
-    }
-  })
-
-  // Create a new FeedbackData object
   const newFeedbackData: FeedbackDataI = {
     requestId: sanitizedRequestId,
     employeeId: sanitizedEmployeeId,
-    answersBySection: answersBySection,
+    answersBySection,
   }
 
   try {
-    // Find the existing feedback data by requestId
     const existingData = await feedbackDataModel.findOne({ requestId: newFeedbackData.requestId })
 
     if (!existingData) {
       throw createHttpError(404, 'Feedback data not found')
     }
 
-    // Merge the new feedback data with the existing data
     newFeedbackData.answersBySection.forEach((newSection) => {
       const { sectionName, score } = newSection
       const existingSection = existingData.answersBySection.find(
@@ -202,15 +175,12 @@ export const updateFeedbackData: RequestHandler<
       )
 
       if (existingSection) {
-        // Add the scores from the new section to the existing section
         existingSection.score.push(...score)
       } else {
-        // Add the new section data to the existing data
         existingData.answersBySection.push(newSection)
       }
     })
 
-    // Save the updated feedback data
     await existingData.save()
     res.sendStatus(200)
   } catch (error) {
