df804f89f17bd0d3aee544aefce706b493c4cddf
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-03 20:11:53 +0300

    Formatting and comments for questions.ts

--- src/controllers/questions.ts
+++ src/controllers/questions.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express'
 import QuestionModel, { SectionI } from '../data_models/questions'
 
+// Get all questions
 export const getAll: RequestHandler = async (req, res, next) => {
   try {
     const questionsAll = await QuestionModel.find().exec()
@@ -10,16 +11,16 @@ export const getAll: RequestHandler = async (req, res, next) => {
   }
 }
 
-
+// Insert questions into the database
 export const insertSections: RequestHandler = async (req, res, next) => {
   try {
-    const sections: SectionI[] = req.body.sections;
+    const sections: SectionI[] = req.body.sections
 
     // Insert sections and questions
-    const insertedSections = await QuestionModel.insertMany(sections);
+    const insertedSections = await QuestionModel.insertMany(sections)
 
-    res.status(200).json(insertedSections);
+    res.status(200).json(insertedSections)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
