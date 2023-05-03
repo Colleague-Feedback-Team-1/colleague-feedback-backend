252150d6b6cfda5c3ed2dbbe13d832144b23acbd
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-03 20:12:41 +0300

    Formatting and comments for feedbackData.ts

--- src/routes/feedbackData.ts
+++ src/routes/feedbackData.ts
@@ -3,12 +3,14 @@ import * as feedbackDataController from '../controllers/feedbackData'
 
 const router = express.Router()
 
-//get all feedback data
+//get all feedback data entries 
 router.get('/', feedbackDataController.getAll)
-//insert feedback data OR update feedback data
+//insert feedback data OR update feedback data  
 router.post('/insert-feedback', feedbackDataController.insertFeedbackData)
+//update feedback data
 router.patch('/insert-feedback/:requestid', feedbackDataController.updateFeedbackData)
 
 //find data by request id
 router.get('/:requestid', feedbackDataController.getFeedbackDataByRequestId)
+
 export default router
