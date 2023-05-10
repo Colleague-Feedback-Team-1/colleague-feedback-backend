2f8017c99f45fb45dc3d5f48bc2b669213144458
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-10 12:03:07 +0300

    Notifications controller

--- src/controllers/notifications.ts
+++ src/controllers/notifications.ts
@@ -1,6 +1,11 @@
 import { RequestHandler } from 'express'
 import createHttpError from 'http-errors'
-import NotificationModel, { NotificationI } from '../data_models/notifications'
+import NotificationModel, {
+  NotificationI,
+  Sender,
+  Receiver,
+  notificationTypeEnum,
+} from '../data_models/notifications'
 import validator from 'validator'
 import mongoose from 'mongoose'
 
@@ -13,15 +18,15 @@ export const getAllNotifications: RequestHandler = async (req, res, next) => {
   }
 }
 
-export const getNotificationsByEmployeeId: RequestHandler = async (req, res, next) => {
-  const employeeid = validator.escape(req.params.employeeid)
+export const getNotificationsByReceiverId: RequestHandler = async (req, res, next) => {
+  const receiverid = validator.escape(req.params.receiverid)
   try {
-    if (!mongoose.Types.ObjectId.isValid(employeeid)) {
-      throw createHttpError(400, `Employee id: ${employeeid} is invalid `)
+    if (!mongoose.Types.ObjectId.isValid(receiverid)) {
+      throw createHttpError(400, `Employee id: ${receiverid} is invalid `)
     }
-    const notifications = await NotificationModel.find({ 'receiver.employeeid': employeeid }).exec()
+    const notifications = await NotificationModel.find({ 'receiver.employeeid': receiverid }).exec()
     if (!notifications) {
-      throw createHttpError(404, `Notifications for ${employeeid} not found`)
+      throw createHttpError(404, `Notifications for ${receiverid} not found`)
     }
     res.status(200).json(notifications)
   } catch (error) {
@@ -34,10 +39,48 @@ export const insertNotification: RequestHandler<unknown, unknown, NotificationI,
   res,
   next
 ) => {
-  const notificationData: NotificationI = req.body
   try {
-    const newNotification = await NotificationModel.create(notificationData)
-    res.status(201).json(newNotification)
+    const { type, date, sender, receiver, requestid } = req.body
+
+    if (!type || !date || !sender || !receiver) {
+      throw createHttpError(400, 'Missing required fields in request body')
+    }
+
+    if (!notificationTypeEnum.includes(type)) {
+      throw createHttpError(400, `Invalid notification type: ${type}`)
+    }
+
+    if (!validator.isDate(date)) {
+      throw createHttpError(400, `Invalid date: ${date}`)
+    }
+
+    if (
+      !Array.isArray(sender) ||
+      sender.length === 0 ||
+      !sender.every((s) => s.senderid && s.senderName)
+    ) {
+      throw createHttpError(400, 'Invalid sender format')
+    }
+
+    if (
+      !Array.isArray(receiver) ||
+      receiver.length === 0 ||
+      !receiver.every((r) => r.receiverid !== undefined && r.receiverName)
+    ) {
+      throw createHttpError(400, 'Invalid receiver format')
+    }
+
+    const notification: NotificationI = {
+      type,
+      date,
+      sender,
+      receiver,
+      requestid: requestid || null,
+    }
+
+    const result = await NotificationModel.create(notification)
+
+    res.status(201).json(result)
   } catch (error) {
     next(error)
   }
