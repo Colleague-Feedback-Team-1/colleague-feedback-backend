3e55c92c88b92a83e81f7af442783630740a932b
Ilia Ponomarenko <ponomarenkoilya989@gmail.com>
2023-05-10 12:04:15 +0300

    Notifications routes

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 import * as notificationController from '../controllers/notifications'
 
-
 const router = express.Router()
 
 router.get('/', notificationController.getAllNotifications)
-router.post("/insert-notification", notificationController.insertNotification)
-
+router.get('/by-receiver/:receiverid', notificationController.getNotificationsByReceiverId)
+router.post('/insert-notification', notificationController.insertNotification)
 
 export default router
